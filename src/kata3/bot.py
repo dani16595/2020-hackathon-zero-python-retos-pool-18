45efda7fbf575e274d370d846990da8599fc3720
dani16595 <dani_16_5_95@hotmail.com>
2020-05-16 17:51:20 +0200

    Bot creado y testeado

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,20 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    update.message.reply_text("Hola, Geeks!")
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    update.message.reply_text("Ayudame!")
 
 def mayus(update, context):
-        #
-        return ""
+    """Envia un mensaje a mayuscula cuando se emita el comando /mayus."""
+    update.message.reply_text(context.args[0].upper())
 
 def alreves(update, context):
-    """Repite el mensaje del usuario."""
+    """Envia un mensaje al reves cuando no se emita un comando."""
     #
-    return ""
+    update.message.reply_text(update.message.text[::-1])
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,18 +34,18 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1155082026:AAHBYlC2LFfNP7s_iLIMj6014Qnz-T3K76M", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp =updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help",help))
+    dp.add_handler(CommandHandler("mayus",mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    dp.add_handler(MessageHandler(filters=Filters.text, callback=alreves))
     
     # Y este espera al error
     dp.add_error_handler(error)
@@ -54,6 +54,7 @@ def main():
     updater.start_polling()
 
     updater.idle()
+
 
 
 if __name__ == '__main__':
